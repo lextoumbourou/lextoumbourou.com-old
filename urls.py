d91eb51f5b88b9387e0ffbb739683bd06d309f9f
Lex Toumbourou <lextoumbourou@gmail.com>
2011-11-26 13:28:11 +1100

    Initial commit

---
+++ urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls.defaults import patterns, include, url
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('lexandstuff.blog.urls')),
+    url(r'^$', include('lexandstuff.blog.urls')),
+	)
