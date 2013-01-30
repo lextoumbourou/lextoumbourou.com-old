ed21eae7c0b6f86366f7a8b9a4d5d41de0b4b440
Lex Toumbourou <lextoumbourou@gmail.com>
2013-01-30 18:27:05 +1100

    Moved static files to correct location

--- lextoumbourou/urls.py
+++ lextoumbourou/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^blog/', include('lexandstuff.blog.urls')),
-    url(r'^$', include('lexandstuff.blog.urls')),
+    url(r'^blog/', include('lextoumbourou.blog.urls')),
+    url(r'^$', include('lextoumbourou.blog.urls')),
 )
