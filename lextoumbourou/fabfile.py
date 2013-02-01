e0c07b4078caaa4220040d0e8c4ed86e3a2bf087
Lex Toumbourou <lextoumbourou@gmail.com>
2013-02-02 10:39:57 +1100

    Move inital_build task into own function

--- lextoumbourou/fabfile.py
+++ lextoumbourou/fabfile.py
@@ -5,6 +5,8 @@ from fabric.contrib import files
 
 import private
 
+GIT_REPO = 'git://github.com/lextoumbourou/lextoumbourou.com.git'
+
 def prod():
     env.hosts = list(private.PROD_SERVERS)
 
@@ -13,18 +15,27 @@ def local():
     env.hosts = ['localhost']
 
 
+def initial_build():
+    """
+    Clone project and set permissions
+    """
+    # Clone project if it doesn't exist
+    with settings(warn_only=True):
+        if run('test -d {0}'.format(private.APP_DIR)).failed:
+            run('git clone {0} {1}'.format(GIT_REPO, private.APP_DIR))
+
+    # Make sure permissions are correct
+    sudo('chown -R {0} {1}'.format(private.USER_GROUP, private.APP_DIR))
+    sudo('chmod -R 775 {0}'.format(private.APP_DIR))
+
+
 def deploy():
     """
     Deploy code to production
     """
-    git_repo = 'git://github.com/lextoumbourou/lextoumbourou.com.git'
-    with settings(warn_only=True):
-        if run('test -d {0}'.format(private.APP_DIR)).failed:
-                run('git clone {0} {1}'.format(git_repo, private.APP_DIR))
-    # Make sure permissions are correct
-    sudo('chown -R {0} {1}'.format(private.USER_GROUP, private.APP_DIR))
-    sudo('chmod -R 775 {0}'.format(private.APP_DIR))
-    # Django app deployment tasks
+    initial_build()
+
+    # Perform Django app deployment tasks
     with cd(private.APP_DIR):
         run('git pull')
         put('private.py', 'lextoumbourou/private.py')
