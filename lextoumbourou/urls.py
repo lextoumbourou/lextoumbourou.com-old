2bd8dcad2cfe6dc358e81bcd44224c4d73c780d4
Lex Toumbourou <lextoumbourou@gmail.com>
2013-03-19 20:37:50 +1100

    Might have been a good idea to test it, or something? Fixed urls

--- lextoumbourou/urls.py
+++ lextoumbourou/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.views.generic.simple import direct_to_template
+
 admin.autodiscover()
 
 urlpatterns = patterns(
