603c2b0b16b22160365b5541e828757bebce4e51
Lex Toumbourou <lextoumbourou@gmail.com>
2013-03-19 20:29:26 +1100

    Robots, human, 404 and 500 added

--- lextoumbourou/urls.py
+++ lextoumbourou/urls.py
@@ -7,4 +7,8 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('lextoumbourou.blog.urls')),
     url(r'^$', include('lextoumbourou.blog.urls')),
+    url(r'^robots.txt', direct_to_template,
+        {'template': 'robots.txt', 'mimetype': 'text/plain'}),
+    url(r'^humans.txt', direct_to_template,
+        {'template': 'humans.txt', 'mimetype': 'text/plain'}),
 )
